63b7d2405d007a8dc22a2d4b785e80befeba942e
hmhv <admin@hmhv.info>
2024-04-28 13:22:59 +0900

    remove SwiftJWT dependency, use CryptoKit to make JWT

--- ASCAPIClient-Example-iOS/ASCJWT.swift
+++ ASCAPIClient-Example-iOS/ASCJWT.swift
@@ -6,31 +6,62 @@
 //
 
 import Foundation
-import SwiftJWT
+import CryptoKit
 
 enum ASCJWT {
     static func generateJWT(keyID: String, issuerID: String, privateKey: String) throws -> String {
         let header = Header(kid: keyID)
-        let claims = ASCClaims(iss: issuerID,
-                              iat: Date(timeIntervalSinceNow: 0),
-                              exp: Date(timeIntervalSinceNow: 1200),
-                              aud: "appstoreconnect-v1")
-        var jwt = JWT(header: header, claims: claims)
-        let jwtSigner = JWTSigner.es256(privateKey: privateKey.data(using: .utf8)!)
+        let payload = Payload(iss: issuerID)
+        let headerAndPayloadString = header.toJWTEncodedString + "." + payload.toJWTEncodedString
+        let headerAndPayloadData = Data(headerAndPayloadString.utf8)
+        
+        let signedToken = try P256.Signing.PrivateKey(pemRepresentation: privateKey)
+            .signature(for: headerAndPayloadData)
+            .rawRepresentation.toBase64JWTEncodedString
+        return headerAndPayloadString + "." + signedToken
+    }
+    
+    private struct Header: Encodable {
+        let alg = "ES256"
+        let kid: String
+        let typ = "JWT"
+    }
 
-        do {
-            return try jwt.sign(using: jwtSigner)
-        } catch {
-            debugPrint("ERROR::JWT \(error.localizedDescription)")
-            throw error
+    private struct Payload: Encodable {
+        let iss: String
+        let iat: Date
+        let exp: Date
+        let aud: String
+        
+        init(iss: String) {
+            let now = Date.now
+            self.iss = iss
+            self.iat = Date(timeInterval: 0, since: now)
+            self.exp = Date(timeInterval: 60 * 20, since: now)
+            self.aud = "appstoreconnect-v1"
         }
     }
 }
 
-struct ASCClaims: Claims {
-    let iss: String
-    let iat: Date
-    let exp: Date
-    let aud: String
+private extension Encodable {
+    var toJWTEncodedString: String {
+        try! JSONEncoder.jwtEncoder.encode(self).toBase64JWTEncodedString
+    }
 }
 
+private extension JSONEncoder {
+    static let jwtEncoder = {
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.dateEncodingStrategy = .secondsSince1970
+        return jsonEncoder
+    }()
+}
+    
+private extension Data {
+    var toBase64JWTEncodedString: String {
+        base64EncodedString()
+            .replacingOccurrences(of: "+", with: "-")
+            .replacingOccurrences(of: "/", with: "_")
+            .replacingOccurrences(of: "=", with: "")
+    }
+}
